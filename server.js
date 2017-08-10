3f1c2cc59bb27872ec34ec8efc99c86250644161
UchihaUzumaki1x <30682419+UchihaUzumaki1x@users.noreply.github.com>
2017-08-10 13:12:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var content = {
+  title: 'Article One | Ankit Profile',
+  heading: 'Aryicle One',
+  date: 'August 10, 2017',
+  content: `<p>Today I started working on my First Webpage.</p>
+            <p>And here It is. My first webpage in front of you.</p>
+                <div>
+                <h4>
+            <p>It was really a fun thing to this task.</p>
+                </h4>`
+}; 
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+             <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req,res) {
@@ -28,6 +76,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
