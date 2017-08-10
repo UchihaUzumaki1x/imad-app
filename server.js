e4d349fd44a4771d2737afec6a16906fabe1a110
UchihaUzumaki1x <30682419+UchihaUzumaki1x@users.noreply.github.com>
2017-08-10 10:13:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,12 +17,8 @@ app.get('/article-two', function (req,res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article-two', function (req,res) {
-  res.send('Article two resquested and will be served here');
-});
-
 app.get('/article-three', function (req,res) {
-  res.send('Article three resquested and will be served here');
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
