84b04f0f8797246fe0e15bfdfe5d4411dcc1f677
UchihaUzumaki1x <30682419+UchihaUzumaki1x@users.noreply.github.com>
2017-08-14 18:47:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,6 +20,6 @@ button.onclick = function() {
     };
     
     // Make the request
-    request.open('GET', 'http://imad.hasura.io/counter', true);
+    request.open('GET', 'http://kumarankitx022.imad.hasura-app.io/counter', true);
     request.send(null);
 };
